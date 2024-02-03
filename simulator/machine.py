a6955306498b2b8f48ae322b46d82a3a2a60f4ab
berniehuang2008 <berniehuang2008@163.com>
2024-02-03 12:55:38 +0800

    "shared machine" to reduce storage useage

--- simulator/machine.py
+++ simulator/machine.py
@@ -1,18 +1,55 @@
 import os
+import io
+import tarfile
+import yaml
 import docker
+import time
 
 
 client = docker.from_env()
 
 
-def run(image: str, ports: dict):
-    container = client.containers.run(image, detach=True, ports=ports)
+def run(image: str, ports: dict, host: dict):
+    container = client.containers.run(image, detach=True, ports=ports, hostname=host["Hostname"])
     return container.id
+
+
+def config_machine(container_id, config):
+    def bash_run(command):
+        container.exec_run(["/bin/bash", "-c", command])
+
+    container = client.containers.get(container_id)
+
+    # init users
+    users = config["users"]
+    for user in users:
+        if user["name"] != "root":
+            bash_run(f"useradd -ms /bin/bash {user['name']}")
+        if "groups" in user:
+            for group in user["groups"]:
+                bash_run(f"addgroup {group}")
+                bash_run(f"adduser {user['name']} {group}")
+        if "password" in user:
+            bash_run(f"echo '{user['name']}:{user['password']}' | chpasswd")
+
+    # Init file
+    tar_stream = io.BytesIO()
+    with tarfile.open(fileobj=tar_stream, mode="w") as tar:
+        tar.add("game/machines/{}/vm/files/".format(config["IPv3"]), arcname=".")
+    tar_stream.seek(0)
+    container.put_archive("/", tar_stream)
 
 
 def start_machine(ipv3, port=10122):
     os.system(f"ssh-keygen -R [localhost]:{str(port)}")
-    container_id = run(f"berniehuang2008/hackerdomination:{ipv3}", {22: port})
+
+    with open("game/machines/{}/vm/config.yaml".format(ipv3)) as f:
+        config = yaml.safe_load(f)
+
+    container_id = run(config["machine"], {22: port}, config["host"])
+
+    config_machine(container_id, config)
+
     return container_id
 
 
