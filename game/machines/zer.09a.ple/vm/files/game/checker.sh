cb0153d58e7737393d8863ba47c4396807e26ff5
berniehuang2008 <berniehuang2008@163.com>
2024-02-10 11:17:34 +0800

    Update scripts for checker and UI in city1

--- game/machines/zer.09a.ple/vm/files/game/checker.sh
+++ game/machines/zer.09a.ple/vm/files/game/checker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-origin_hash="root:\$6\$Z4.wjAeraoZ2KfVJ\$yVkNLGRjUVSFD1qQoEYqrSbj4mWjWueZfjqaqcunLzSh2R4pq1Z2yA9rHHwZnotNlH2kYGRt7XUQGic3SZbVD1:19755:0:99999:7:::"
+origin_hash=$(sudo cat /game/origin_hash.dat)
 now_hash=$(sudo grep root /etc/shadow)
 
 if [ "$origin_hash" != "$now_hash" ]; then
